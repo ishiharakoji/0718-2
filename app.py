8334e3f7d8fc0be646f2eb55e49672cb1102881e
ishiharakoji <ishihara.koji@btech.jp>
2024-07-18 18:26:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def communicate():
         messages=messages
     )
 
-    bot_message = response["choices"][0]["messages"]
+    bot_message = response["choices"][0]["message"]
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
