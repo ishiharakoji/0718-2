e70e32b567142403530b0840bbe5454e8e1bf5b6
ishiharakoji <ishihara.koji@btech.jp>
2024-07-18 18:29:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-
 import streamlit as st
-from openai import OpenAI
+import openai
 
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
-openai_api_key = st.secrets.OpenAIAPI.openai_api_key
-
-client = OpenAI(api_key=openai_api_key)
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
@@ -20,7 +17,7 @@ def communicate():
     user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
 
-    response = client.chat.completions.create(
+    response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
@@ -32,10 +29,10 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("ノベオカ AIアシスタント")
-st.write("AIがあなたの質問にお応えします。")
+st.title("My AI Assistant")
+st.write("ChatGPT APIを使ったチャットボットです。")
 
-user_input = st.text_input("ここに質問を入力してください", key="user_input", on_change=communicate)
+user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
